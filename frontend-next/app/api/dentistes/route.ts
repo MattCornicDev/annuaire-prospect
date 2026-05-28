61339d0286b9d727c5c83d469045ad86d5911278
MattCornicDev <glgharib@gmail.com>
2026-05-29 01:35:03 +0200

    Ajout dossier api
    Signed-off-by: MattCornicDev <glgharib@gmail.com>

---
+++ frontend-next/app/api/dentistes/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const url =
+      "https://api.insee.fr/entreprises/sirene/V3/siret?q=activitePrincipaleUniteLegale:8623Z AND codePostalEtablissement:59*&nombre=2000";
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.INSEE_TOKEN}`,
+      },
+    });
+
+    const data = await res.json();
+
+    const formatted = data.etablissements.map((e: any) => ({
+      _displayName: e.uniteLegale.denominationUniteLegale,
+      adresseEtablissement: e.adresseEtablissement,
+      siret: e.siret,
+      website: null,
+      email: null,
+      phone: null,
+    }));
+
+    return NextResponse.json(formatted);
+  } catch (err) {
+    return NextResponse.json({ error: "Erreur API" }, { status: 500 });
+  }
+}
